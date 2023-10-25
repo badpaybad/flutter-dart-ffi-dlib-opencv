f7cd5316fe497d383f55b7f812e74c2c3d3468a1
Nguyen Phan Du <badpaybad@gmail.com>
2023-10-25 22:17:25 +0700

    ndk fix build c++, manual install flutter dont use snap

--- lib/interop/dlib.cpp
+++ lib/interop/dlib.cpp
@@ -3,6 +3,16 @@
 //#include "../../android/app/src/main/cppLibs/dlib/include/dlib/dlib/gui_widgets.h"
 #include "../../android/app/src/main/cppLibs/dlib/include/dlib/dlib/image_io.h"
 #include <iostream>
+#include "../../android/app/src/main/cppLibs/dlib/include/dlib/dlib/dnn.h"
+#include "../../android/app/src/main/cppLibs/dlib/include/dlib/dlib/data_io.h"
+#include "../../android/app/src/main/cppLibs/dlib/include/dlib/dlib/image_processing.h"
+
 
 using namespace dlib;
 using namespace std;
+
+int detect_face(){
+    frontal_face_detector detector = get_frontal_face_detector();
+
+    return 0;
+}
